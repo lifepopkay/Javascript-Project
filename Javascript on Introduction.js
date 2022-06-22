016c5e333847dbd2ccef29d8cbe035d9b73d6699
lifepopkay <popoolafolas@gmail.com>
2022-06-22 16:38:20 +0100

    Javascript task

---
+++ Javascript on Introduction.js
@@ -0,0 +1,17 @@
+const firstName = 'Kayode'
+const lastName = ' Popoola'
+
+const FullName = firstName + lastName
+
+console.log(FullName)
+
+let value = 160
+let heightUnit = ' cm'
+
+let height = value + heightUnit
+console.log(height)
+
+let Country = 'Nigeria'
+console.log(Country)
+
+
